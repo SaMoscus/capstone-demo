8aaea3633a4aa2845b858402c2e4676fdd717f15
SaMoscus <moscusodom@gmail.com>
2026-07-24 16:33:17 +0100

    Add terraform foundation files

---
+++ infra/terraform/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2"
+    }
+  }
+}
